431783d4863d8ab4ec75b270c34a39deafb1eeba
Alparslan <alparslanaydgn02@gmail.com>
2023-05-01 17:28:29 +0300

    Algorithm is done but have a simple bug. I'll fix it.

--- src/main/java/com/example/Knapsack/Main.java
+++ src/main/java/com/example/Knapsack/Main.java
@@ -5,25 +5,24 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-	
-		String path = "C:/campspring/Knapsack/src/main/java/com/example/Knapsack/ks_19_0";
-		
+
+		String path = "C:/campspring/Knapsack/src/main/java/com/example/Knapsack/ks_4_0";
+
 		ReadFromFile readFromFile = new ReadFromFile();
-		
+
 		ArrayList<Integer> values = readFromFile.getValues(path);
 		ArrayList<Integer> weights = readFromFile.getWeights(path);
 
+		SolveKnapsack solveKnapsack = new SolveKnapsack(values, weights);
 
-		
-		SolveKnapsack solveKnapsack = new SolveKnapsack(values,weights);
 		int[] usedItems = solveKnapsack.solveKnapsack();
 		for (int i = 0; i < usedItems.length; i++) {
-			System.out.println(usedItems[i]);
+			System.out.print(usedItems[i] + " ");
 		}
-		
-		
-//		System.out.println(solveKnapsack.optimalValue);
-//		optimal value değerini ekrana bas sonra foreaach ile return edilen diziyi gez
-		
+		System.out.println(" ");
+
+		System.out.println("Optimum Value: " + solveKnapsack.optimalValue);
+		System.out.println("Total Weights: " + solveKnapsack.totalWeight);
+
 	}
 }
--- src/main/java/com/example/Knapsack/SolveKnapsack.java
+++ src/main/java/com/example/Knapsack/SolveKnapsack.java
@@ -6,72 +6,66 @@ public class SolveKnapsack {
 
 	ArrayList<Integer> values;
 	ArrayList<Integer> weights;
-	
+
 	public SolveKnapsack(ArrayList<Integer> values, ArrayList<Integer> weights) {
 		this.values = values;
 		this.weights = weights;
-	}
-	
-	int[] usedItems;
-	int optimalValue=0;
-	
-	int maxWeight;
-	int totalWeight=0;
-	
-	
-	
-	public int[] solveKnapsack() {
-		maxWeight = weights.get(0); 
-
-		
 		createUsedItems();
-		
-		int largestItem = largestItem();//values.get(largestItem) Value/weight değeri en büyük item
-		
-		// bir döngü veya fonksiyon içinde largest item aklenmeli =>add(largestItem);
-		// daha sonra values den o değer silinmeli veya sıfıra vs işitlenmeli
-		//sonra en büyük item tekrar belirlenmeli ve bu fonksiyon tekrar çağrılmalı
-		//bu fonksiyonun özelliği ise totalWeight<maxWeight olasıya kadar çalışmalı. 
-		//son aşamada totalWeight < max weight olursa o largest item de kaldırılmalı
-		//veya totalweight < weight[i] ile total weighti en az olanlara bakılmalı
-		add(largestItem);
-		
-		
-		
+	}
+
+	int[] usedItems;
+	int optimalValue = 0;
+
+	int maxWeight;
+	int totalWeight = 0;
+
+	public int[] solveKnapsack() {
+		maxWeight = weights.get(0);
+		algorithmKnapsack();
 		return usedItems;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	public void add(int a) {
-		usedItems[a-1] = 1;
-	}
-	
-	
-	public void createUsedItems() {
-		usedItems = new int[values.get(0)];
-		for (int i = 0; i < values.get(0); i++) {
-			usedItems[i]=0;
+
+	public void algorithmKnapsack() {
+
+		while (values.size() > 1) {// Text dosyasındaki ilk değerler kalacak(Toplam ağırlık ve eşya sayısı)
+			int largestItem = largestItem();// values.get(largestItem) Value/weight değeri en büyük item
+
+			if (totalWeight < maxWeight && weights.get(largestItem) < maxWeight - totalWeight) {
+				add(largestItem - 1);
+				optimalValue = optimalValue + values.get(largestItem);
+				totalWeight = totalWeight + weights.get(largestItem);
+				values.remove(largestItem);
+				weights.remove(largestItem);
+			} else {
+				values.remove(largestItem);
+				weights.remove(largestItem);
+			}
+
 		}
 	}
-	
-	
-	public int largestItem() {// Value/weight değeri en büyük itemi buluyor
+
+	public void add(int a) {// Alınan ürünleri 1 yaptığımız dizi
+		usedItems[a] = 1;
+	}
+
+	public void createUsedItems() {// Diziyi oluşturup sıfır atıyor
+		usedItems = new int[values.get(0)];
+		for (int i = 0; i < values.get(0); i++) {
+			usedItems[i] = 0;
+		}
+	}
+
+	public int largestItem() {// Value/weight değeri en büyük itemi buluyor. Index numarasını dönüyor
 		double maxRatio = 0.0;
 		int maxIndex = -1;
-		
-		for (int i = 1; i <= values.get(0); i++) {
+
+		for (int i = 1; i <= values.size() - 1; i++) {
 			double ratio = (double) values.get(i) / weights.get(i);
 			if (ratio > maxRatio) {
 				maxRatio = ratio;
 				maxIndex = i;
-			} 
+			}
 		}
-		return maxIndex;	
+		return maxIndex;
 	}
 }
